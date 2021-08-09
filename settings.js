607a06df4a46e61f1143122c7899f4b35e88adb3
micahosborne <micahosborne@gmail.com>
2021-08-08 21:39:35 -0400

    Dropdown stuff

--- settings.js
+++ settings.js
@@ -1,1 +1,33 @@
-/home/micahosborne/Documents/gSnap/settings.js
+'use strict'
+// Library imports
+const Gio = imports.gi.Gio;
+
+// Extension imports
+const Me = imports.misc.extensionUtils.getCurrentExtension();
+
+var settings;
+
+function get() {
+  if (!settings) {
+    let dir = Me.dir.get_child('schemas').get_path();
+    let source = Gio.SettingsSchemaSource.new_from_directory(
+        dir,
+        Gio.SettingsSchemaSource.get_default(),
+        false);
+
+    if (!source) {
+        throw new Error('Error Initializing the thingy.');
+    }
+
+    let schema = source.lookup('org.gnome.shell.extensions.gsnap', false);
+
+    if (!schema) {
+        throw new Error('Schema missing.');
+    }
+
+    settings = new Gio.Settings({
+        settings_schema: schema
+    });
+  }
+  return settings;
+}
